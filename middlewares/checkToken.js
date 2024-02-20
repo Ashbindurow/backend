b1f47f0f11db766565dc083e2b478f614a951335
Ashbindurow <ashbindurow97@gmail.com>
2024-02-20 13:41:31 +0530

    ADDING AUTHORIZATION MIDDLEWARE

---
+++ middlewares/checkToken.js
@@ -0,0 +1,21 @@
+import Jwt from "jsonwebtoken";
+
+const checkToken = (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(403).json({ message: "You are not authorized" });
+    }
+
+    const ogToken = token.split(" ")[1];
+    //token number will be recieve on the 1st index position
+
+    const isValid = Jwt.verify(ogToken, process.env.USER_SECRET_KEY);
+    next();
+  } catch (e) {
+    return res.status(403).json({ message: "You are not authorized" });
+  }
+};
+
+export default checkToken;
